e51624733ab1907980404c2256bf8d87539a2476
Bhavya8290 <marepallibhavyasre@gmail.com>
2025-08-06 22:30:16 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,29 @@
+script.js
+const myInput = document.getElementById('myInput');
+const goButton = document.getElementById('goButton');
+const myTaskBoard = document.getElementById('myTaskBoard');
+goButton.addEventListener('click', insertTask);
+myInput.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') insertTask();
+});
+function insertTask(){
+    const taskText = myInput.value.trim();
+    if (taskvalue === '') return;
+const taskItem = document.createElement('li');
+  taskItem.className = 'itemBox';
+  taskItem.innerHTML = `
+    <span>${taskValue}</span>
+    <button onclick="eraseTask(this)">🗑</button>
+  `;
+  taskItem.addEventListener('click', function (e) {
+    if (e.target.tagName !== 'BUTTON') {
+      taskItem.classList.toggle('checked');
+    }
+  });
+  myTaskBoard.appendChild(taskItem);
+  myInput.value = '';
+}
+function eraseTask(button) {
+  const item = button.closest('li');
+  item.remove();
+}
